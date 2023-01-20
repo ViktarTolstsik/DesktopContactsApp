91115e8d32689c3bfd2a37119e87834d6299fc06
Viktar Tolstsik <viktor.tolstik.96@gmail.com>
2023-01-20 11:55:36 +0300

    Добавьте файлы проекта.

---
+++ DesktopContactsApp/NewContactWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DesktopContactsApp
+{
+    /// <summary>
+    /// Логика взаимодействия для NewContactWindow.xaml
+    /// </summary>
+    public partial class NewContactWindow : Window
+    {
+        public NewContactWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
