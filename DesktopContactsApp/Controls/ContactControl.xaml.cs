b49afa523de8d350b97780daa5f3f0cfc9c38bde
Viktar Tolstsik <viktor.tolstik.96@gmail.com>
2023-01-25 09:41:05 +0300

    .

--- DesktopContactsApp/Controls/ContactControl.xaml.cs
+++ DesktopContactsApp/Controls/ContactControl.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DesktopContactsApp.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,6 +21,20 @@ namespace DesktopContactsApp.Controls
     /// </summary>
     public partial class ContactControl : UserControl
     {
+        private Contact contact;
+
+        public Contact Contact
+        {
+            get { return contact; }
+            set 
+            { 
+                contact = value;
+                nameTextBlock.Text = contact.Name;
+                phoneTextBlock.Text = contact.Phone;
+                emailTextBlock.Text = contact.Email;
+            }
+        }
+
         public ContactControl()
         {
             InitializeComponent();
